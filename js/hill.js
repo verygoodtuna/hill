8209b9fe0e031063485a8115272e56494440fecc
verygoodtuna <kjy990504@gmail.com>
2023-07-18 18:19:11 +0900

    -v-

--- js/hill.js
+++ js/hill.js
@@ -12,11 +12,24 @@ $(function () {
 
     const PremiumSlide = new Swiper ('.premium8_slide', {
         loop: true,
+        //freeMode: true,
         spaceBetween: 30,
         slidesPerView: 4,
+        slidesPerGroup: 4,
+        
     });
 
     const UnitSlide = new Swiper ('.unit_slide', {
         loop: true,
+        slidesPerView: '3',
+        slidesPerGroup: '3',
+        spaceBetween: 15,
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        //   },
+        loopAdditionalSlides : 1,
+        freeMode : false,
+        autoHeight : true,
     })
 })
