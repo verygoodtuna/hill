2d05ba9fada0bbe265a9e69ffb2720a2ad99a98b
verygoodtuna <kjy990504@gmail.com>
2023-07-20 12:27:03 +0900

    -3-

--- js/hill.js
+++ js/hill.js
@@ -10,6 +10,26 @@ $(function () {
         loop: true,
     });
 
+    $('.menu_btn').on('click', function () {
+      $('.header .gnb').toggleClass('on');
+    })
+
+    $('.gnb ul>li>a').on('click', function () {
+      $('.gnb ul>li .lnb').toggleClass('on');
+    })
+
+    $('.gnb ul>li .gnb_list').on('click', function (e) {
+      if ($('.gnb').hasClass('on')) {
+
+          //서버메뉴가 없으면 바로 클릭되게 하기
+          if ($(this).next().size() != 0) {
+              e.preventDefault();
+          }
+          $(this).next().stop().slideToggle();
+          $(this).parent().siblings().find('.lnb').stop().slideUp();
+      }
+    });
+
     $('.premium8_slide').slick ({
         arrows:false,
         slidesToShow: 4,
